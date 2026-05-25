b033dbe8bb1220f4ef7c146399120076588c054c
Michael DAS <40369786+mdsiha@users.noreply.github.com>
2026-05-25 11:21:37 +0300

    feat: export FakeMercure and error classes from package entrypoint

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@
 |--------------------------------------------------------------------------
 | Package entrypoint
 |--------------------------------------------------------------------------
-|
-| Export values from the package entrypoint as you see fit.
-|
 */
 export { configure } from './configure.js'
 export { Mercure } from './src/mercure.js'
+export { FakeMercure } from './src/fake_mercure.js'
 export { defineConfig } from './src/define_config.js'
 export { stubsRoot } from './stubs/main.js'
+export { MercureError } from './src/errors/mercure_error.js'
+export { MercurePublishError } from './src/errors/publish_error.js'
+export { MercureConfigError } from './src/errors/config_error.js'
